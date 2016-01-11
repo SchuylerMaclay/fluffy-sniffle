da61664dfb91fad1e17a1292a7f3e520b024c7fa
SchuylerMaclay <schuylermaclay@gmail.com>
2016-01-11 10:45:55 -0500

    followship added

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,18 @@ class User < ActiveRecord::Base
   validates :username, presence: true
   validates :username, length: { minimum: 1 }
 
+  has_many :follower_relationships, classname: "Followship", foreign_key: "followed_id"
+  has_many :followed_relationships, classname: "Followship", foreign_key: "follower_id"
+
+  has_many :followers, through: :follower_relationships
+  has_many :followeds, through: :followed_relationships
+
   has_many :qweets
+
+  def following?(user)
+    self.followeds.include?(user)
+  end
+
+  def follow(user)
+    Relationship.create(follower_id: self.id, followed_id: user.id)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :followships
   root to: 'users#index'
   get 'registrations/new'
 
